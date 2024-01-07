c447b91b229753879a9c0648c2ae02110040afc9
Sam Johnson <sambjohnson94@gmail.com>
2024-01-06 19:32:24 -0800

    remove unneeded early-init

--- early-init.el
+++
@@ -1,8 +0,0 @@
-;;; init.el --- Emacs init script -*- lexical-binding: t -*-
-;;; Commentary:
-;;; This file loads when Emacs first boots up
-;;; Code:
-(setq package-enable-at-startup nil)
-
-(provide 'early-init)
-;;; early-init.el ends here
