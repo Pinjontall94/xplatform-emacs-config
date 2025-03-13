f8f10316e750afc89f6b9dc10789b0e647358153
Sam Johnson <sambjohnson94@gmail.com>
2025-03-13 12:15:14 -0700

    add zen command

--- lisp/hello.el
+++ lisp/hello.el
@@ -2,4 +2,12 @@
   (interactive)
   (princ "hello! :3"))
 
+(defun zen ()
+  (interactive)
+  (if (or tool-bar-mode menu-bar-mode)
+      (progn ;; (tool-bar-mode 0)
+	     (menu-bar-mode 0))
+    (progn ;; (tool-bar-mode 1)
+	   (menu-bar-mode 1))))
+
 (provide 'hello)
