ebfa334cab427f7b477937de1cedf1833c8aba80
Sam Johnson <sambjohnson94@gmail.com>
2025-03-13 12:02:22 -0700

    fix misc-conf formatting, add and enable dired-auto-readme

--- lisp/misc-conf.el
+++ lisp/misc-conf.el
@@ -1,8 +1,23 @@
-;;   ===============================================================
-;;  == Miscellaneous (Stuff I don't want to clog up the init.el) ==
-;; ===============================================================
+;;; misc-conf.el --- Miscellaneous (Stuff I don't want to clog up the init.el)
 
+;;; Commentary:
+
+;;; Code:
 ;; show the readme in dired buffers
-(use-package dired-auto-readme :ensure t)
+(use-package dired-auto-readme :ensure t
+  :config
+  (dired-auto-readme-mode t))
+
+(use-package impatient-mode :ensure t :defer t)
+(use-package simple-httpd :ensure t :defer t)
+(use-package hl-todo :ensure t
+  :config
+  (global-hl-todo-mode t))
+
+(defun config-emacs ()
+  "This opens the Emacs config folder for easy editing."
+  (interactive)
+  (dired "~/.emacs.d/"))
 
 (provide 'misc-conf)
+;;; misc-conf.el ends here.
