d29d711b6faa5cc68f2ec4b782390366bad43320
Sammi Johnson <sambjohnson94@protonmail.com>
2025-05-13 15:46:43 -0700

    simplify lisp conf, rm roswell dependencies, add ecl to slime-impls

--- lisp/lisp-conf.el
+++ lisp/lisp-conf.el
@@ -2,21 +2,23 @@
 ;;; Commentary:
 ;;; Code:
 ;; Common Lisp 👽
-(add-to-list 'load-path "~/lisp/slime/")
-;;(load (expand-file-name "~/.roswell/helper.el"))
-(require 'slime-autoloads)
-(setq inferior-lisp-program "ros -Q run")
-(setq-default slime-contribs '(slime-fancy slime-company))
-(setq-default slime-lisp-implementations
-	      `((sbcl ("sbcl" "--dynamic-space-size" "4Gb" "--core" "sbcl.core-for-slime"))
-		(roswell ("ros" "-L" "sbcl" "-Q" "run" "--" "--noinform" "--dynamic-space-size" "4Gb"))))
-(setf slime-default-lisp 'roswell)
+(use-package slime :defer
+  :config
+  (add-to-list 'load-path "~/.emacs.d/elpa/slime/")
+  (require 'slime-autoloads)
+  (setq inferior-lisp-program "sbcl")
+  (setq-default slime-contribs '(slime-fancy slime-company))
+  (setq-default slime-lisp-implementations
+		`((sbcl ("sbcl" "--core" "sbcl.core-for-slime"))
+		  (ecl ("ecl"))))
+  (setf slime-default-lisp 'sbcl))
 
-;; Automatically connect to slime when opening a lisp file
-;; (add-hook 'slime-mode-hook
-;;                (lambda ()
-;;                  (unless (slime-connected-p)
-;;                    (save-excursion (slime)))))
+  ;; Automatically connect to slime when opening a lisp file
+  ;; (add-hook 'slime-mode-hook
+  ;;                (lambda ()
+  ;;                  (unless (slime-connected-p)
+  ;;                    (save-excursion (slime)))))
+  )
 
 (provide 'lisp-conf)
 ;;; lisp-conf.el ends here
